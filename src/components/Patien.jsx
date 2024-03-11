6042b63afc9637851128d684fc9df8368ab14919
Simply92 <pedrouner92@gmail.com>
2024-03-11 16:07:44 -0300

    estilos

--- src/components/Patien.jsx
+++ src/components/Patien.jsx
@@ -11,26 +11,26 @@ const Patien = ({ paciente, setPaciente, eliminarPaciente }) => {
     }
   };
   return (
-    <div className="m-3 shadow-md px-5 py-10 rounded-xl bg-blue-400 md:w-1/2 lg:w-2/3">
+    <div className="m-3 shadow-md px-5 py-10 rounded-xl bg-blue-200 md:w-1/2 lg:w-2/3">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Nombre: {""}
-        <span className="font-normal normal-case">{nombre}</span>
+        <span className="normal-case font-bold">{nombre}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Propietario: {""}
-        <span className="font-normal normal-case">{propietario}</span>
+        <span className="font-bold normal-case">{propietario}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Email: {""}
-        <span className="font-normal normal-case">{email}</span>
+        <span className="font-bold normal-case">{email}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Fecha Alta: {""}
-        <span className="font-normal normal-case">{fecha}</span>
+        <span className="font-bold normal-case">{fecha}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Sintomas: {""}
-        <span className="font-normal normal-case">{sintomas}</span>
+        <span className="font-bold normal-case">{sintomas}</span>
       </p>
 
       <div className="flex justify-between mt-10">
